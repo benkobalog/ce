eb82c47e38babd7577a53b87710295baa92b2a54
Your Name <benko.balog@gmail.com>
2017-07-12 19:57:50 +0200

    Change to REST API instead of scraping

--- project.clj
+++ project.clj
@@ -5,7 +5,8 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [clj-http "3.6.1"]
-                 [hickory "0.7.1"]]
+                 [hickory "0.7.1"]
+                 [org.clojure/data.json "0.2.6"]]
   :main ^:skip-aot ce.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- src/ce/core.clj
+++ src/ce/core.clj
@@ -1,52 +1,38 @@
 (ns ce.core
   (:gen-class)
   (:require [clj-http.client :as client]
-            [hickory.select :as s])
+            [clojure.data.json :as json])
   (:use hickory.core))
 
-(def content (-> (client/get "https://www.currenciesdirect.com/en/currency-tools/today-s-rate") :body parse as-hickory))
+(def body
+  (json/read-str
+    ((client/get "http://api.fixer.io/latest")
+      :body )))
 
-(def getCurrencyPairsRaw
-  (s/select
-      (s/descendant
-        (s/class  "js-todaysrates-rows")
-        (s/tag :strong))
-        content))
+(def baseCurrency (body "base"))
+(def date (body "date"))
+(def rates
+  (assoc
+    (body "rates")
+    "EUR" 1.0 ))
 
-(def getRatesRaw
-  (s/select
-    (s/descendant
-      (s/class "js-todaysrates-rows")
-      (s/class "rates"))
-    content))
-
-
-(def ratesList
-  (map read-string
-     (map first
-        (map :content getRatesRaw))))
-
-(def currencyPairList
-  (partition 2 2
-     (map first
-        (map :content getCurrencyPairsRaw))))
-
+(defn convert
+  [quantity from to]
+  (* (/ (rates to) (rates from)) quantity))
 
 (defn -main
-  "I don't do a whole lot ... yet."
+  "--"
   [& args]
 
   ;; parse parameters
 
   ;; do conversion
-  (clojure.pprint/pprint  ratesList)
-  (clojure.pprint/pprint  currencyPairList)
-  (clojure.pprint/pprint  (+ (nth ratesList 3) (nth ratesList 4)))
+  (println baseCurrency)
+  (println date)
+  (println (rates "HUF"))
+  ;(println (* (rates "HUF") (rates "EUR") 123))
+  (println (convert 1000 "USD" "HUF" ))
+)
 
-
-  ;;(clojure.pprint/pprint  (map vector currencyPairList ratesList))
-
-
-  (println "End of program"))
 
 
